216375484314c5d53600ecaafbd86b58ec76cbfa
Kalid Mohammed <126605249+kalid1738@users.noreply.github.com>
2026-02-01 11:48:33 +0300

    Add files via upload

---
+++ database/about.php
@@ -0,0 +1,17 @@
+
+
+
+
+<div class="content py-3">
+    <div class="card rounded-0 card-outline card-navy shadow" >
+        <div class="card-body rounded-0">
+            <h1 class="text-center">ABOUT US</h1>
+            <center><hr class="bg-navy border-navy w-25 border-2"></center>
+            <div>
+            <b> <center>Welcome to the Multi seller e-market system where you can buy or sale anything online.</center></b>
+               <b> <p align="center">This Website is developed by Kalid Mohammed </p></b>
+               
+            </div>
+        </div>
+    </div>
+</div>
